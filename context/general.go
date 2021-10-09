bb9436141fe747c6a5fc3500ddf5001b7dc1b18c
LuckyG0ldfish <tim-koeneke@online.de>
2021-10-09 16:15:58 +0200

    amf context done

---
+++ context/general.go
@@ -0,0 +1,23 @@
+package context 
+
+import (
+	"net"
+	
+
+	"github.com/ishidawataru/sctp"
+)
+
+func GenSCTPAddr(ip string, port int) (lbAddr *sctp.SCTPAddr, err error){
+	ips := []net.IPAddr{}
+	if ip, err1 := net.ResolveIPAddr("ip", ip); err1 != nil {
+		//err := fmt.Errorf("Error resolving address '%s': %v", ip, err1)
+		return nil, err1
+	} else {
+		ips = append(ips, *ip)
+	}
+	lbAddr = &sctp.SCTPAddr{
+		IPAddrs: ips,
+		Port:    port,
+	}
+	return lbAddr, nil
+}
