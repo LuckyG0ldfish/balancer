778e5e461c392c14693fcca9132447cc9e77d712
LuckyG0ldfish <tim-koeneke@online.de>
2021-10-09 16:14:29 +0200

    amf context done

--- context/lb_amf.go
+++ context/lb_amf.go
@@ -1,5 +1,51 @@
 package context
 
+import (
+	"time"
+
+	"github.com/ishidawataru/sctp"
+)
+
+const lbPPID uint32 = 0x3c000000
+
 type LbAmf struct{
-	
+	amfID 		int 
+	Conn 		*sctp.SCTPConn
+}
+
+func NewLbAmf(id int) (amf *LbAmf){
+	amf.amfID = id
+	amf.Conn = nil 
+	return amf
+}
+
+func (amf *LbAmf) Start(lbaddr sctp.SCTPAddr, amfIP string, amfPort int) {
+	for{
+		err := amf.ConnectToAmf(lbaddr, amfIP, amfPort)
+		if err == nil {
+			// amf.up = true
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func (amf *LbAmf) ConnectToAmf(lbaddr sctp.SCTPAddr, amfIP string, amfPort int) error{
+	amfAddr, _ := GenSCTPAddr(amfIP, amfPort)
+	conn, err := sctp.DialSCTP("sctp", &lbaddr, amfAddr)
+	if err != nil {
+		return  err
+	}
+	info, err := conn.GetDefaultSentParam()
+	if err != nil {
+		return  err
+	}
+	info.PPID = lbPPID
+	err = conn.SetDefaultSentParam(info)
+	if err != nil {
+		return  err
+	}
+	//setting this connection as the amf SCTPConn
+	amf.Conn = conn
+	return  nil
 }
