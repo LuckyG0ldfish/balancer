fa84fe1c500e97244f7330942a31d62b16b951b1
LuckyG0ldfish <tim-koeneke@online.de>
2021-10-17 15:30:51 +0200

    adopt structure

--- go.mod
+++ go.mod
@@ -8,12 +8,18 @@ require (
 	github.com/antonfisher/nested-logrus-formatter v1.3.0
 	github.com/free5gc/amf v1.3.0
 	github.com/free5gc/aper v1.0.1
+	github.com/free5gc/fsm v1.0.0
+	github.com/free5gc/http2_util v1.0.0
 	github.com/free5gc/logger_util v1.0.0
 	github.com/free5gc/nas v1.0.1
 	github.com/free5gc/ngap v1.0.2
 	github.com/free5gc/openapi v1.0.0
+	github.com/free5gc/path_util v1.0.0
+	github.com/free5gc/version v1.0.0
+	github.com/gin-contrib/cors v1.3.1
 	github.com/ishidawataru/sctp v0.0.0-20210707070123-9a39160e9062
 	github.com/sirupsen/logrus v1.8.1
+	github.com/urfave/cli v1.22.5
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
 )
---
+++ lb.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+
+	"github.com/LuckyG0ldfish/balancer/logger"
+	"github.com/LuckyG0ldfish/balancer/service"
+	"github.com/free5gc/version"
+)
+
+var LB = &service.LB{}
+
+var appLog *logrus.Entry
+
+func init() {
+	appLog = logger.AppLog
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "amf"
+	appLog.Infoln(app.Name)
+	appLog.Infoln("AMF version: ", version.GetVersion())
+	app.Usage = "-free5gccfg common configuration file -amfcfg amf configuration file"
+	app.Action = action
+	app.Flags = LB.GetCliCmd()
+	if err := app.Run(os.Args); err != nil {
+		appLog.Errorf("AMF Run error: %v", err)
+		return
+	}
+}
+
+func action(c *cli.Context) error {
+	if err := LB.Initialize(c); err != nil {
+		// logger.CfgLog.Errorf("%+v", err)
+		return fmt.Errorf("Failed to initialize !!")
+	}
+
+	LB.Start()
+
+	return nil
+}
